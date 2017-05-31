8acf872e7ec039d70f89538f1aadb66e2565f0dd
luismoramedina <luismoramedina@gmail.com>
2017-05-31 18:34:57 +0200

    add another commented http calls, port = 3000

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,8 +9,18 @@
  */
 angular.module('myYoProjectApp')
   .controller('MainCtrl', function ($scope, $http) {
+//No http model
 //    $scope.tasks = ['task 1', '2', '3'];
-      $http.get('http://localhost:9000/tasks.json').success(
+//With cookie
+//      var url = 'http://cookie-gateway-local.dev.corp:8080/cards'
+//      var config = {withCredentials: true}
+//With cookie local
+//      var url = 'https://cookie-gateway.dev.corp/locations/address/mdb/count'
+//      var config = {withCredentials: true}
+      var url = 'http://localhost:3000/tasks.json'
+      var config = {}
+      $http.get(url, config
+         ).success(
          function(data) {
             $scope.tasks = data;
          }
